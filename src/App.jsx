734de18337898125d7c9adc9c3b120a77cdd9792
Aquib848 <aquibraza958@gmail.com>
2026-05-03 01:09:00 +0530

    resolve the conflict of header with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
-
+      <div className="pt-32 md:pt-28">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/packages" element={<Packages />} />
@@ -21,7 +21,7 @@ function App() {
         <Route path="/blog/:id" element={<BlogDetail />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-
+      </div>
       <FloatingButtons /> 
       <Footer />
     </BrowserRouter>
